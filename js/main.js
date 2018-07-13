da089caae3ed5ed0c372a708460a8a07f2be1404
Steve Bunin <steveone@gmail.com>
2018-07-13 14:04:06 -0700

    Updated restaurantname from h1 to h2

--- js/main.js
+++ js/main.js
@@ -177,7 +177,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(picture);
 
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.className = 'restaurantName';
   name.innerHTML = restaurant.name;
   name.id = restaurant.name;
